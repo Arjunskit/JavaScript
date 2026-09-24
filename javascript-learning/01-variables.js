4d69f6bbbf0a3731d53ef586257b9c24a3808d90
Arjunskit <sk.arjun.it@gmail.com>
2026-09-24 12:42:58 +0530

    JS Variables

---
+++ javascript-learning/01-variables.js
@@ -0,0 +1,50 @@
+// make a small Student Profile by using Js variables:
+
+// Name
+// Age
+// Course
+// College
+// Current role
+// Number of projects
+// Is currently learning JavaScript? 
+
+const studentname = "Arjun S K"
+let age = 25
+const course ="B.Tech IT"
+let role = "Fullstack Developer"
+let projectcount = 6
+let jslearningstatus = "Yes"
+
+
+// studentname = "Rahul" // TypeError: Assignment to constant variable.
+// console.log("studentname")
+
+// let age = 30 // SyntaxError: Identifier 'age' has already been declared
+// console.log(age) 
+
+// projectcount++
+// console.log(projectcount) // 7 , Value Changed
+
+// console.log("----- Student Profile -----")
+// console.log("Name: ",studentname) // unchangeable value
+// console.log("Age: ", age) // changeable
+// console.log("Course: ", course) // unchangeable
+// console.log("Role: ", role) // changeable
+// console.log("Projects: ", projectcount) // changeable
+// console.log("Learning JavaScript: ", jslearningstatus) // changeable
+// console.log("--------------------------")
+
+
+// console.log(x)  // ReferenceError: Cannot access 'x' before initialization
+// let x
+
+// console.log(y) // ReferenceError: Cannot access 'y' before initialization
+// const y = 10
+
+
+// console.log(z) // undefined
+// var z
+
+// console.log(a) // undefined
+// var a = 10
+
